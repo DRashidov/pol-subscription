722bbc394ae6c46448ff10f6dd9c3d89909acff6
DRashidov <rashydov.work@gmail.com>
2020-08-03 11:31:58 +0300

    add react context

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
 import { NavBar, Footer } from './components';
 import { Home, Profile } from './views';
+import { UserContext } from './context/user';
 
 import './app.css';
 
 const App = () => {
+  const [authTokens, setAuthTokens] = useState('');
+
   return (
+    <UserContext.Provider value={{ authTokens, setAuthTokens }}>
     <div id='app' className='d-flex flex-column h-100'>
       <NavBar />
       <Container className='flex-grow-1 mt-5'>
@@ -20,6 +24,7 @@ const App = () => {
       </Container>
       <Footer />
     </div>
+    </UserContext.Provider>
   );
 };
 
